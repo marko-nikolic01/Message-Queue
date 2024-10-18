d34099deeaaffc41359f712059db9304a6ae2830
marko-nikolic01 <nikolicmarko1243@gmail.com>
2024-10-18 15:29:20 +0200

    feat: Created channel structure header file.

---
+++ message_queue/channel.h
@@ -0,0 +1,13 @@
+#ifndef CHANNEL_H
+#define CHANNEL_H
+
+#include "message_queue.h"
+
+typedef struct Channel {
+    MessageQueue queue;
+    char name[50];
+} Channel;
+
+void initChannel(Channel *channel, const char *name);
+
+#endif
